e64b5cb805d5ec31cbe3d34b23a2078251fe5a02
nguyenhuy <huynguyen@tmon.co.kr>
2019-01-07 17:55:33 +0700

    add kafka client setup

--- scraper-estate/src/main/java/com/scraper/app/repositories/api/UserRepository.java
+++
@@ -1,4 +0,0 @@
-package com.scraper.app.repositories.api;
-
-public interface UserRepository {
-}
--- scraper-estate/src/main/java/com/scraper/app/repositories/impl/UserRepositoryImpl.java
+++
@@ -1,9 +0,0 @@
-package com.scraper.app.repositories.impl;
-
-import com.scrap.app.repositories.api.UserRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class UserRepositoryImpl implements UserRepository {
-
-}
